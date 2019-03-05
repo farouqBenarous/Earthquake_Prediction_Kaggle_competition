781dd7211f8b35b0b8df019ba40158225d6a0ed1
benarous farouk <benarousfarouk@new-host.home>
2019-03-05 14:02:13 +0000

    Bringing data

---
+++ demo(ElasticNet Regression).py
@@ -0,0 +1,102 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+from sklearn import datasets
+from tensorflow.python.framework import ops
+import pandas as pd
+
+
+# function for plotting based on both features
+def plot_acc_ttf_data(Acousticdata, timeFailuer,
+                      title="Acoustic data and time to failure: 1% sampled data"):
+    fig, ax1 = plt.subplots(figsize=(12, 8))
+
+    plt.title(title)
+    plt.plot(Acousticdata, color='r')
+    ax1.set_ylabel('acoustic data', color='r')
+    plt.legend(['acoustic data'], loc=(0.01, 0.95))
+
+    ax2 = ax1.twinx()
+    plt.plot(timeFailuer, color='b')
+    ax2.set_ylabel('time to failure', color='b')
+    plt.legend(['time to failure'], loc=(0.01, 0.9))
+    plt.grid(True)
+    del Acousticdata
+    del timeFailuer
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    # ---------------------------- Bringing the data--------------------------------------
+    pd.set_option("display.precision", 13)
+    data = pd.read_csv("Data/train.csv", nrows=5000,
+                       dtype={'acoustic_data': np.int16, 'time_to_failure': np.float64}, float_precision="hight")
+
+    # Acousticdata = data.loc[:, ["acoustic_data"]].values
+    Acousticdata = list(x for x in data["acoustic_data"])
+    Acousticdata = np.array(Acousticdata).reshape(5000, 1)
+    # timeFailuer = data.loc[:, ["time_to_failure"]].values
+    timeFailuer = list(x for x in data["time_to_failure"])
+    timeFailuer = np.array(timeFailuer).reshape(5000, 1)
+
+    print(Acousticdata)
+    plot_acc_ttf_data(Acousticdata, timeFailuer, title="Acoustic data and time to failure: 1% sampled data")
+
+    # -------------------------- Start buildin and training my model ----------------------
+    # Create graph
+
+    tf.reset_default_graph()
+    sess = tf.Session()
+    # make results reproducible
+    seed = 13
+    np.random.seed(seed)
+    tf.set_random_seed(seed)
+
+    # Declare batch size
+    batch_size = 50
+
+    # Initialize placeholders
+    x_data = tf.placeholder(shape=(5000, None), dtype=tf.float32)
+    y_target = tf.placeholder(shape=(5000, None), dtype=tf.float32)
+
+    # Create variables for linear regression
+    A = tf.Variable(tf.random_normal(shape=[1, 1]))
+    b = tf.Variable(tf.random_normal(shape=[1, 1]))
+
+    # Declare model operations
+    model_output = tf.add(tf.matmul(x_data, A), b)
+
+    # Declare the elastic net loss function
+    elastic_param1 = tf.constant(1.)
+    elastic_param2 = tf.constant(1.)
+    l1_a_loss = tf.reduce_mean(tf.abs(A))
+    l2_a_loss = tf.reduce_mean(tf.square(A))
+    e1_term = tf.multiply(elastic_param1, l1_a_loss)
+    e2_term = tf.multiply(elastic_param2, l2_a_loss)
+
+    # define the loss function and the optimazer
+    loss = tf.expand_dims(tf.add(tf.add(tf.reduce_mean(tf.square(y_target - model_output)), e1_term), e2_term), 0)
+    optimizer = tf.train.GradientDescentOptimizer(0.001).minimize(loss)
+
+    # Initialize variabls and tensorflow session
+    sess.run(tf.global_variables_initializer())
+
+    # Training loop
+    loss_vec = []
+    for i in range(1000):
+
+        sess.run(optimizer, feed_dict={x_data: Acousticdata, y_target: timeFailuer})
+        if (i + 1) % 250 == 0:
+            temp_loss = sess.run(loss, feed_dict={x_data: Acousticdata, y_target: timeFailuer})
+            loss_vec.append(temp_loss[0])
+            print('Step #' + str(i + 1) + ' A = ' + str(sess.run(A)) + ' b = ' + str(sess.run(b)))
+            print('Loss = ' + str(temp_loss))
+
+    answer = tf.equal(tf.floor(model_output), y_target)
+    print(answer)
+    accuracy = tf.reduce_mean(tf.cast(answer, "float32"))
+    print(accuracy)
+    print("Accuracy: ", accuracy.eval({x_data: Acousticdata, y_target: timeFailuer}, session=sess) * 100, "%")
+
+
--- demo.py
+++
@@ -1,1 +0,0 @@
-print ("Still working on ")
